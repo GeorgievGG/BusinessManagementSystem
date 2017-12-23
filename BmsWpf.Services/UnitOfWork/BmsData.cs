eb9b1e5d04fec907e98aaab4172be1f32cd228d0
GeorgievGG <Juvetikon@abv.bg>
2017-12-23 23:25:51 +0200

    Implement Services .NET Core library and implement Repository & UnitOfWork classes

---
+++ BmsWpf.Services/UnitOfWork/BmsData.cs
@@ -0,0 +1,86 @@
+﻿using BMS.DataBaseData;
+using BMS.DataBaseModels;
+using BmsWpf.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace BmsWpf.Services.UnitOfWork
+{
+    public class BmsData : IBmsData
+    {
+        private readonly DbContext dbContext;
+
+        private readonly IDictionary<Type, object> repositories;
+
+        public BmsData()
+            : this(new BmsContex())
+        {
+        }
+
+        public BmsData(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.repositories = new Dictionary<Type, object>();
+        }
+
+        public IRepository<Contragent> Contragents
+        {
+            get { return this.GetRepository<Contragent>(); }
+        }
+
+        public IRepository<Inquiry> Inquiries
+        {
+            get { return this.GetRepository<Inquiry>(); }
+        }
+
+        public IRepository<Invoice> Invoices
+        {
+            get { return this.GetRepository<Invoice>(); }
+        }
+
+        public IRepository<Note> Notes
+        {
+            get { return this.GetRepository<Note>(); }
+
+        }
+
+        public IRepository<Offer> Offers
+        {
+            get { return this.GetRepository<Offer>(); }
+        }
+
+        public IRepository<Payment> Payments
+        {
+            get { return this.GetRepository<Payment>(); }
+        }
+
+        public IRepository<Project> Projects
+        {
+            get { return this.GetRepository<Project>(); }
+        }
+
+        public IRepository<User> Users
+        {
+            get { return this.GetRepository<User>(); }
+        }
+
+        public void SaveChanges()
+        {
+            this.dbContext.SaveChanges();
+        }
+
+        private IRepository<T> GetRepository<T>() where T : class
+        {
+            if (!this.repositories.ContainsKey(typeof(T)))
+            {
+                var type = typeof(GenericEfRepository<T>);
+                this.repositories.Add(
+                    typeof(T),
+                    Activator.CreateInstance(type, this.dbContext));
+            }
+
+            return (IRepository<T>)this.repositories[typeof(T)];
+        }
+    }
+}
---
+++ BmsWpf.Services/UnitOfWork/IBmsData.cs
@@ -0,0 +1,27 @@
+﻿using BMS.DataBaseModels;
+using BmsWpf.Services.Repositories;
+
+namespace BmsWpf.Services.UnitOfWork
+{
+
+    public interface IBmsData
+    {
+        IRepository<Contragent> Contragents { get; }
+
+        IRepository<Inquiry> Inquiries { get; }
+        
+        IRepository<Invoice> Invoices { get; }
+        
+        IRepository<Note> Notes { get; }
+        
+        IRepository<Offer> Offers { get; }
+        
+        IRepository<Payment> Payments { get; }
+        
+        IRepository<Project> Projects { get; }
+        
+        IRepository<User> Users { get; }
+        
+        void SaveChanges();
+    }
+}
