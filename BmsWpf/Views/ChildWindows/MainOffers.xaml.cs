043d39fa2b7e83b39783a60d270b52b51d555616
iarnaudov91 <iarnaudov91@gmail.com>
2017-12-27 09:00:19 +0200

    m

--- BmsWpf/Views/ChildWindows/MainOffers.xaml.cs
+++ BmsWpf/Views/ChildWindows/MainOffers.xaml.cs
@@ -1,6 +1,10 @@
-﻿using BmsWpf.Views.Forms;
+﻿using BMS.DataBaseData;
+using BMS.DataBaseModels;
+using BmsWpf.Views.Forms;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,16 +36,64 @@ namespace BmsWpf.Views.ChildWindows
             this.Close();
         }
 
-        private void Edit_Click(object sender, RoutedEventArgs e)
+        private void Back_click(object sender, RoutedEventArgs e)
         {
-            var dash = new OfferForm();
+            var dash = new MainWindow();
             dash.Show();
             this.Close();
         }
 
-        private void Back_click(object sender, RoutedEventArgs e)
+        public void fillingDataGrid()
         {
-            var dash = new MainWindow();
+            var db = new BmsContex();
+            DataTable dt = new DataTable();
+            DataColumn creator = new DataColumn("Creator", typeof(string));
+            DataColumn client = new DataColumn("Client", typeof(string));
+            DataColumn inquiry = new DataColumn("InquiryID", typeof(string));
+            DataColumn description = new DataColumn("Description", typeof(string));
+            DataColumn date = new DataColumn("Date", typeof(DateTime));
+
+            dt.Columns.Add(creator);
+            dt.Columns.Add(client);
+            dt.Columns.Add(inquiry);
+            dt.Columns.Add(description);
+            dt.Columns.Add(date);
+
+            var offers = db.Offers.Include(e => e.Client).Include(e => e.Creator).Include(e => e.Inquiry).ToArray();
+
+            foreach (var offer in offers)
+            {
+                DataRow row = dt.NewRow();
+                row[0] = offer.Creator.Username;
+                row[1] = offer.Client.Name;
+                row[2] = offer.Inquiry.Description;
+                row[3] = offer.Description;
+                row[4] = offer.Date;
+                dt.Rows.Add(row);
+            }
+
+
+            OffersDataGrid.ItemsSource = dt.DefaultView;
+        }
+
+        private void OffersDataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.fillingDataGrid();
+        }
+
+        private void OffersDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var dash = new OfferForm();
+            var dada = this.OffersDataGrid.SelectedItems as Offer;
+  
+
+            dash.Show();
+            this.Close();
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            var dash = new OfferForm();
             dash.Show();
             this.Close();
         }
--- BmsWpf/Views/Forms/OfferForm.xaml.cs
+++ BmsWpf/Views/Forms/OfferForm.xaml.cs
@@ -1,6 +1,9 @@
-﻿using BmsWpf.Views.ChildWindows;
+﻿using BMS.DataBaseData;
+using BMS.DataBaseModels;
+using BmsWpf.Views.ChildWindows;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,9 +26,58 @@ namespace BmsWpf.Views.Forms
         public OfferForm()
         {
             InitializeComponent();
+            fillCombos();
+
         }
+
+        private void fillCombos()
+        {
+            var db = new BmsContex();
+
+            var clients = db.Contragents.ToList();
+            ClientCombo.ItemsSource = clients.Select(c => c.Name);
+
+            var inquiries = db.Inquiries.ToList();
+            InquiryCombo.ItemsSource = inquiries.Select(c => c.Description);
+
+            var creators = db.Users.ToList();
+            CreatorCombo.ItemsSource = creators.Select(c => c.Username);
+
+        }
+
         private void Back_Click(object sender, RoutedEventArgs e)
         {
+            var dash = new MainOffers();
+            dash.Show();
+            this.Close();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            var db = new BmsContex();
+            var client_form = ClientCombo.SelectedItem.ToString();
+            var inquiry_form = InquiryCombo.SelectedItem.ToString();
+            var creator_form = CreatorCombo.SelectedItem.ToString();
+            var desc_form = offer_description.Text;
+            var date_form = date.SelectedDate.ToString(); // 6.12.2017 г. 0:00:00
+            var format = "d.MM.yyyy г. h:mm:ss";
+
+            var clientId = db.Contragents.Where(u => u.Name == client_form).SingleOrDefault().Id;
+            var creatorId = db.Users.Where(u => u.Username == creator_form).SingleOrDefault().Id;
+            var inquiryId = db.Inquiries.Where(u => u.Description == inquiry_form).SingleOrDefault().Id;
+
+            var newOffer = new Offer
+            {
+                ClientId = clientId,
+                CreatorId = creatorId,
+                InquiryId = inquiryId,
+                Description = desc_form,
+                Date = DateTime.ParseExact(date_form, format, CultureInfo.InvariantCulture)
+            };
+            db.Offers.Add(newOffer);
+            db.SaveChanges();
+            MessageBox.Show("Offer Successfully created!");
+
             var dash = new MainOffers();
             dash.Show();
             this.Close();
