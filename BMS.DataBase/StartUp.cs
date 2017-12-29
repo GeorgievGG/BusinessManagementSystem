9ad3c53bffc2ba9b96958996366c45aa41f083a2
iarnaudov91 <iarnaudov91@gmail.com>
2017-12-29 14:08:33 +0200

    Inquiry and Offer forms DONE

--- BMS.DataBase/StartUp.cs
+++ BMS.DataBase/StartUp.cs
@@ -12,10 +12,12 @@ namespace BMS.DataBase
         {
             using (var context = new BmsContex())
             {
-                context.Database.EnsureDeleted();
-                context.Database.EnsureCreated();
-                Seed();
+               context.Database.EnsureDeleted();
+               context.Database.EnsureCreated();
+               Seed();
+
             }
+        
         }
         // enter in the admin panel with:
         // Username: admin
@@ -65,11 +67,11 @@ namespace BMS.DataBase
 
             var contragents = new[]
              {                                               
-                new Contragent {PersonalVatNumber = "SD12351",PersonalIndentityNumber = "101", BankDetails="BGNKJ1453STZ", Telephone = "0879665233", Address= "Hristo Botev 207", Description = "nai dobrata firma", Email = "", Name = "Kaufland AD" , PersonForContact= "Vladimir", },                                                          
-                new Contragent {PersonalVatNumber = "DFA1235",PersonalIndentityNumber = "102", BankDetails="UKLDNSF1523SF", Telephone = "0889655294", Address= "Peicho Slaveikov 3", Description = "nai dobrata firma", Email = "", Name = "WeSupply EOOD", PersonForContact = "Pesho"},                                                           
-                new Contragent {PersonalVatNumber = "DAS1233",PersonalIndentityNumber = "103", BankDetails="DESF1523STZ", Telephone = "0879615259", Address= "Raina Knqginq 24", Description = "nai dobrata firma", Email = "", Name = "Goods EOOD", PersonForContact = "Ivan"}, 
-                new Contragent {PersonalVatNumber = "DS12357",PersonalIndentityNumber = "104", BankDetails="USASF1523STZ", Telephone = "0879665232", Address= "Biznes Park Sofia", Description = "nai dobrata firma", Email = "", Name = "Market2U OOD", PersonForContact = "Georgi"},                                                            
-                new Contragent {PersonalVatNumber = "DJG1236",PersonalIndentityNumber = "105", BankDetails="MEXSF1523STZ", Telephone = "0879665275", Address= "Tsar Simeon Veliki 137", Description = "nai dobrata firma", Email = "", Name = "Billa AD", PersonForContact = "Vladimir"},
+                new Contragent {PersonalVatNumber = "SD12351",PersonalIndentityNumber = "101", BankDetails="BGNKJ1453STZ", Telephone = "0879665233", Address= "Hristo Botev 207", Description = "nai dobrata firma", Email = "abv@gmail.com", Name = "Kaufland AD" , PersonForContact= "Vladimir", },                                                          
+                new Contragent {PersonalVatNumber = "DFA1235",PersonalIndentityNumber = "102", BankDetails="UKLDNSF1523SF", Telephone = "0889655294", Address= "Peicho Slaveikov 3", Description = "nai dobrata firma", Email = "hugo@abv.bg", Name = "WeSupply EOOD", PersonForContact = "Pesho"},                                                           
+                new Contragent {PersonalVatNumber = "DAS1233",PersonalIndentityNumber = "103", BankDetails="DESF1523STZ", Telephone = "0879615259", Address= "Raina Knqginq 24", Description = "nai dobrata firma", Email = "namaste@gmail.com", Name = "Goods EOOD", PersonForContact = "Ivan"}, 
+                new Contragent {PersonalVatNumber = "DS12357",PersonalIndentityNumber = "104", BankDetails="USASF1523STZ", Telephone = "0879665232", Address= "Biznes Park Sofia", Description = "nai dobrata firma", Email = "koki@yahoo.com", Name = "Market2U OOD", PersonForContact = "Georgi"},                                                            
+                new Contragent {PersonalVatNumber = "DJG1236",PersonalIndentityNumber = "105", BankDetails="MEXSF1523STZ", Telephone = "0879665275", Address= "Tsar Simeon Veliki 137", Description = "nai dobrata firma", Email = "bobi.v@abv.bg", Name = "Billa AD", PersonForContact = "Vladimir"},
             };                                                 
 
             context.Contragents.AddRange(contragents);
@@ -112,8 +114,8 @@ namespace BMS.DataBase
             var project = new[]
             {
                 new Project { ClientId = 1, CreatorId = 2, OfferId = 1, StartDate = DateTime.Now ,EndDate = DateTime.Now,DeadLine = DateTime.Now, InquiryId = 1, Name = "Android App"},
-                new Project { ClientId = 2, CreatorId = 3, OfferId = 2, StartDate = DateTime.Now ,DeadLine = DateTime.Now, InquiryId = 2, Name = "Desktop App"},                                           
-                new Project { ClientId = 3, CreatorId = 4, OfferId = 3, StartDate = DateTime.Now ,DeadLine = DateTime.Now, InquiryId = 3, Name = "Web App"},                                              
+                new Project { ClientId = 2, CreatorId = 3, OfferId = 2, StartDate = DateTime.Now ,EndDate = DateTime.Now,DeadLine = DateTime.Now, InquiryId = 2, Name = "Desktop App"},                                           
+                new Project { ClientId = 3, CreatorId = 4, OfferId = 3, StartDate = DateTime.Now ,EndDate = DateTime.Now,DeadLine = DateTime.Now, InquiryId = 3, Name = "Web App"},                                              
                 new Project { ClientId = 4, CreatorId = 5, OfferId = 4, StartDate = DateTime.Now ,EndDate = DateTime.Now,DeadLine = DateTime.Now, InquiryId = 4, Name = "Sports WebSite"},                                       
                 new Project { ClientId = 5, CreatorId = 6, OfferId = 5, StartDate = DateTime.Now ,EndDate = DateTime.Now,DeadLine = DateTime.Now, InquiryId = 5, Name = "Production Management System"}
             };
--- BmsWpf/Views/Forms/InquireForm.xaml.cs
+++ BmsWpf/Views/Forms/InquireForm.xaml.cs
@@ -1,6 +1,11 @@
-﻿using BmsWpf.Views.ChildWindows;
+﻿using BMS.DataBaseData;
+using BMS.DataBaseModels;
+using BmsWpf.Views.ChildWindows;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,10 +28,87 @@ namespace BmsWpf.Views.Forms
         public InquireForm()
         {
             InitializeComponent();
+            fillCombos();
+        }
+
+        private void fillCombos()
+        {
+            var db = new BmsContex();
+
+            var clients = db.Contragents.ToList();
+            ClientCombo.ItemsSource = clients.Select(c => c.Name);
+
+            var creators = db.Users.ToList();
+            CreatorCombo.ItemsSource = creators.Select(c => c.Username);
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
         {
+            var dash = new MainInquiries();
+            dash.Show();
+            this.Close();
+        }
+
+        private void ClientCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var db = new BmsContex();
+            var inqu = db.Inquiries.Include(i => i.Client).ToList();
+            var clients = db.Contragents.ToList();
+
+            var clientName = ClientCombo.SelectedItem.ToString();
+            var currentClient = clients.Where(i => i.Name == clientName).SingleOrDefault();
+            poc_form.Text = currentClient.PersonForContact;
+            email_form.Text = currentClient.Email;
+            phone_form.Text = currentClient.Telephone;
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            var db = new BmsContex();
+            var client_form = ClientCombo.SelectedItem.ToString();
+            var creator_form = CreatorCombo.SelectedItem.ToString();
+            var desc_form = description_form.Text;
+            var date_form = date.SelectedDate.ToString(); // 6.12.2017 г. 0:00:00
+            var format = "d.MM.yyyy г. h:mm:ss";
+
+            if (date_form == string.Empty)
+            {
+                MessageBox.Show("Please insert date");
+                return;
+            }
+            if (desc_form == string.Empty)
+            {
+                MessageBox.Show("Please insert description");
+                return;
+            }
+
+            var clientId = db.Contragents.Where(u => u.Name == client_form).SingleOrDefault().Id;
+            var creatorId = db.Users.Where(u => u.Username == creator_form).SingleOrDefault().Id;
+            var inquiryId = inquiry_id.Text;
+
+            if (inquiryId == string.Empty)
+            {
+                var newInquiry = new Inquiry
+                {
+                    ClientId = clientId,
+                    CreatorId = creatorId,
+                    Description = description_form.Text,
+                    Date = DateTime.ParseExact(date_form, format, CultureInfo.InvariantCulture)
+                };
+                db.Inquiries.Add(newInquiry);
+                db.SaveChanges();
+                MessageBox.Show("Inquiry Successfully created!");
+            }
+            else
+            {
+                var currentInquiry = db.Inquiries.Where(o => o.Id.ToString() == inquiryId).SingleOrDefault();
+                currentInquiry.ClientId = clientId;
+                currentInquiry.CreatorId = creatorId;
+                currentInquiry.Date = DateTime.ParseExact(date_form, format, CultureInfo.InvariantCulture);
+                currentInquiry.Description = description_form.Text;
+                db.SaveChanges();
+                MessageBox.Show("Inquiry Edited successfully!");
+            }
             var dash = new MainInquiries();
             dash.Show();
             this.Close();
