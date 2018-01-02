cf3453cdc68008bb0b39a123856f0a45fb03cf20
GeorgievGG <Juvetikon@abv.bg>
2018-01-02 22:23:35 +0200

    Merged some diffs

--- BMS.DataBaseData/ServerConfiguration.cs
+++ BMS.DataBaseData/ServerConfiguration.cs
@@ -2,10 +2,6 @@
 {
     public class ServerConfiguration
     {
-<<<<<<< HEAD
-        public static string ConfigurationString = @"Server=DESKTOP-A95RGCJ\SQLEXPRESS;Database=BMS;Integrated Security=True;";
-=======
         public static string ConfigurationString = @"Server=.;Database=Bms;Integrated Security=True;";
->>>>>>> origin/master
     }
 }
