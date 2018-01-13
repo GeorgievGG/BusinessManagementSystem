a876174035c7ec1cfab2542403936a289525f003
mariakancheva <lactacaramel@gmail.com>
2018-01-13 19:36:34 +0200

    Note
    In progress

--- BmsWpf.Services/Services/NoteService.cs
+++ BmsWpf.Services/Services/NoteService.cs
@@ -1,11 +1,16 @@
 ﻿namespace BmsWpf.Services.Services
 {
+    using System;
+
     using BmsWpf.Services.Contracts;
     using BmsWpf.Services.DTOs;
     using MoreLinq;
     using System.Collections.Generic;
     using System.Data;
+    using System.Data.SqlClient;
     using System.Linq;
+
+    using Microsoft.EntityFrameworkCore;
 
     public class NoteService : INoteService
     {
@@ -71,5 +76,66 @@
 
             return last5Notes;
         }
+
+        public string Delete(int id)
+        {
+            var note = this.bmsData.Notes.Find(id);
+
+            try
+            {
+                this.bmsData.Notes.Remove(note);
+                this.bmsData.SaveChanges();
+            }
+            catch (DbUpdateException dbEx)
+            {
+
+                var innerException = dbEx.InnerException;
+                if (innerException is SqlException)
+                {
+                    var sqlEx = (SqlException)innerException;
+                    if (sqlEx.Errors.Count > 0)
+                    {
+                        throw new InvalidOperationException("You cannot delete this event!");
+                    }
+                }
+                throw dbEx;
+            }
+            return $"You deleted note {note.Id} from {note.Date} successfully";
+        }
+        
+
+        public string EditCalendarEvent(NotePostDto newNote)
+        {
+            var noteToUpdate = this.bmsData.Notes.Find(newNote.Id);
+            noteToUpdate.Type = newNote.Type;
+            noteToUpdate.Date = newNote.Date;
+            noteToUpdate.Description = newNote.Description;
+            noteToUpdate.ProjectId = newNote.ProjectId;
+
+            this.bmsData.Notes.Update(noteToUpdate);
+            this.bmsData.SaveChanges();
+
+            return $"Note was updated";
+
+        }
+
+        public string CreateNote(NotePostDto newNote)
+        {
+            var userSvr = new UserService(this.bmsData);
+
+            var note = new Note()
+                           {
+                               Id = newNote.Id,
+                               NoteType = newNote.Type,
+                               Date = newNote.Date,
+                               Description = newNote.Description,
+                               ProjectId = newNote.ProjectId
+                           };
+            this.bmsData.Notes.Add(note);
+            this.bmsData.SaveChanges();
+
+            return $"Note was saved!";
+        }
+
     }
 }
